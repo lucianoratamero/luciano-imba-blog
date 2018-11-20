c9fa518b5c5a2a1f9b7c0779b4dd84b9cfd287aa
Luciano Ratamero <luciano@ratamero.com>
2018-11-20 20:53:26 -0200

    adds basic configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.imba$/,
-				loader: 'imba/loader',
-			}
-		]
-	},
-	resolve: {
-		extensions: [".imba",".js", ".json"]
-	},
-	entry: "./src/client.imba",
-	output: {  path: __dirname + '/dist', filename: "client.js" }
-}
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.imba$/,
+        loader: 'imba/loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.imba', '.js', '.json', '.scss']
+  },
+  plugins: [ new HtmlWebpackPlugin({
+    title: 'luciano@ratamero.com - olá Marilene!'
+  }) ],
+  entry: __dirname + '/src/App.imba',
+  output: { path: __dirname + '/dist', filename: 'app.js' }
+};
